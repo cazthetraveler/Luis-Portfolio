adaa516bd1bdae05aa4634857c50103feb3125f7
MultiMouse <olmos.luisd@outlook.com>
2024-03-01 22:44:31 -0600

    added thumbnail images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,25 +1,39 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Project({projects, category}) {
-    const filterProjects = projects.filter((project) => project.category === category)
-    return (
-        <section className="project-display">
-            {filterProjects.map((project) => {
-                return(
-                    <figure className="project-card" key={project.id}>
-                        <img src={project.image.source} alt={project.image.alt} />
-                        <div className="overlay">
-                            <figcaption>{project.title}</figcaption>
-                            <div className="project-links">
-                            {project.links.repo && <Link target="_blank" to={project.links.repo}><i className="fa-brands fa-github"></i></Link>}
-                                {project.links.deployed && <Link target="_blank" to={project.links.deployed}><i className="fa-solid fa-arrow-up-right-from-square"></i></Link>}
-                            </div>
-                        </div>
-                    </figure>
-                )
-            })}
-        </section>
-    )
+function Project({ projects, category }) {
+  const filterProjects = projects.filter(
+    (project) => project.category === category
+  );
+  return (
+    <section className="project-display">
+      {filterProjects.map((project) => {
+        return (
+          <figure
+            title={project.description}
+            className="project-card"
+            key={project.id}
+          >
+            <img src={project.image.source} alt={project.image.alt} />
+            <div className="overlay">
+              <figcaption>{project.title}</figcaption>
+              <div className="project-links">
+                {project.links.repo && (
+                  <Link target="_blank" to={project.links.repo}>
+                    <i className="fa-brands fa-github"></i>
+                  </Link>
+                )}
+                {project.links.deployed && (
+                  <Link target="_blank" to={project.links.deployed}>
+                    <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </figure>
+        );
+      })}
+    </section>
+  );
 }
-  
+
 export default Project;
